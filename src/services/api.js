c2a324493651de75ca1e2692cfb29505724cf7d9
Abdullah Khan <abdullah.khan1010@gmail.com>
2026-08-18 18:00:07 +0500

    Set backend API URL back to https://d3k6lzr995rwjd.cloudfront.net

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,6 @@
 const envBackendUrl = import.meta.env.VITE_BACKEND_URL;
 const API_BASE_URL = (
-    (envBackendUrl && !envBackendUrl.includes('vercel.app') && !envBackendUrl.includes('cloudfront.net'))
-        ? envBackendUrl
-        : 'https://dept-priced-viewing-into.trycloudflare.com'
+    envBackendUrl || 'https://d3k6lzr995rwjd.cloudfront.net'
 ).replace(/\/$/, '');
 
 const buildUrl = (path) => {
