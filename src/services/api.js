7077fac5731b14280c0ed51b69a9f79317f94306
Livox91 <livox1180@gmail.com>
2026-08-21 03:24:27 +0500

    something

--- src/services/api.js
+++ src/services/api.js
@@ -40,12 +40,17 @@ function setStoredCache(url, data) {
 }
 
 async function fetchAndParse(url, options) {
+    const headers = {
+        ...(options.headers || {}),
+    };
+
+    if (options.body && !headers['Content-Type']) {
+        headers['Content-Type'] = 'application/json';
+    }
+
     const response = await fetch(url, {
-        headers: {
-            'Content-Type': 'application/json',
-            ...(options.headers || {}),
-        },
         ...options,
+        headers,
     });
 
     if (!response.ok) {
